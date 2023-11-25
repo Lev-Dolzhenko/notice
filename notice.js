9580b7731f6a317bb30100a5d171b0c688fa2e91
Leo <workspace2707@yandex.ru>
2023-11-25 21:30:38 +0300

    created part of script

--- notice.js
+++ notice.js
@@ -15,3 +15,83 @@
 //         editMode = !editMode;
 //     })
 // }
+
+const titleInput = document.querySelector('.create-block__inputs input');
+const areaInput = document.querySelector('.create-block__inputs textarea');
+const dateInput = document.querySelector('.date-block input');
+const listOfNoteses = document.querySelector('.set-notices');
+const addButton = document.querySelector('.create-block__button button');
+
+document.querySelector('.set-notices').addEventListener('click', function (event) {
+    if (event.target.className === 'image-edit') {
+        let clickedEditButton = event.target;
+        let parentContainer = clickedEditButton.closest('.set-notices__item'); // Находим контейнер с помощью closest
+        let titleInput = parentContainer.querySelector('h3'); // Находим заголовок в контейнере
+        let descriptionInput = parentContainer.querySelector('.description'); // Находим описание в контейнере
+        let clickedCheckButton = parentContainer.querySelector('.image-check');
+        console.log('Click')
+        // let flag = false;
+        // if (flag) {
+        //     clickedCheckButton.classList.remove('none')
+        //     clickedEditButton.classList.add('none')
+        //     titleInput.removeAttribute('contentEditable');
+        //     descriptionInput.removeAttribute('contentEditable');
+        // } else {
+        //     clickedCheckButton.classList.add('none')
+        //     clickedEditButton.classList.remove('none')
+        //     titleInput.setAttribute('contentEditable', true);
+        //     descriptionInput.setAttribute('contentEditable', true);
+        // }
+        // flag = !flag
+    }
+});
+
+addButton.addEventListener('click', function () {
+    createNote(titleInput.value, areaInput.value, dateInput.value)
+})
+
+function createNote(title, description, date) {
+    let containerNote = document.createElement('div');
+    containerNote.classList.add('set-notices__item')
+    let titleNote = document.createElement('h3');
+    titleNote.textContent = title;
+    containerNote.appendChild(titleNote);
+    let descriptionNote = document.createElement('p');
+    descriptionNote.classList.add('description')
+    descriptionNote.textContent = description;
+    containerNote.appendChild(descriptionNote);
+    let containerDate = document.createElement('div');
+    containerDate.classList.add('date');
+    let currentDate = document.createElement('p');
+    let createDate = new Date();
+    let day = createDate.getDate();
+    let month = createDate.getMonth() + 1;
+    let year = createDate.getFullYear();
+    let formattedDate = `${year}-${day}-${month}`;
+    currentDate.textContent = formattedDate;
+    let executeDate = document.createElement('p');
+    executeDate.textContent = date;
+    containerDate.appendChild(currentDate);
+    containerDate.appendChild(executeDate);
+    containerNote.appendChild(containerDate);
+    let imageEdit = document.createElement('img');
+    imageEdit.classList.add('image-edit')
+    imageEdit.src = '/img/icon-edit.png';
+    containerNote.appendChild(imageEdit)
+    let imageCheck = document.createElement('img');
+    imageCheck.classList.add('image-check')
+    imageCheck.src = '/img/icon-check.png';
+    imageCheck.classList.add('none')
+    containerNote.appendChild(imageCheck)
+
+    listOfNoteses.appendChild(containerNote)
+}
+
+
+
+
+
+
+
+
+
