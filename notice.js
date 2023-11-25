05376ad6d4d0a12ebdfbeb6fb35f776ca52153a8
Leo <workspace2707@yandex.ru>
2023-11-25 12:31:26 +0300

    created html&css part

---
+++ notice.js
@@ -0,0 +1,17 @@
+// const allTexts = document.querySelectorAll('.container p');
+// const allButtons = document.querySelectorAll('.container button');
+
+// for (let i = 0; i < allButtons.length; i++) {
+//     let flag = false;
+//     let currButton = allButtons[i]
+//     currButton.addEventListener('click', function() {
+//         if (flag) {
+//             currButton.textContent = 'Edit';
+//             allTexts[i].removeAttribute('contentEditable');
+//         } else {
+//             currButton.textContent = 'Ok'
+//             allTexts[i].setAttribute('contentEditable', true);
+//         }
+//         editMode = !editMode;
+//     })
+// }
